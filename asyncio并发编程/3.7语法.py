b51257bedf09c46481835996460f50313c54356b
new name <my@163.com>
2019-09-29 03:00:09 +0800

    just study

--- asyncio并发编程/3.7语法.py
+++ asyncio并发编程/3.7语法.py
@@ -3,18 +3,37 @@ import asyncio
 
 
 # Python3.7
+import platform
+
+
 async def nested():
     return 42
+
+
+python_version = platform.python_version().rpartition(".")[0]
 
 
 async def main():
     # Schedule nested() to run soon concurrently
     # with "main()".
-    task = asyncio.create_task(nested())
+    # task = asyncio.create_task(nested())
+
+    if float(python_version) >= 3.7:
+        print("3.7版本")
+        task = asyncio.create_task(nested())
+    else:
+        print("3.7以下版本")
+        task = asyncio.ensure_future(nested())
+    print(task)
 
     # "task" can now be used to cancel "nested()", or
     # can simply be awaited to wait until it is complete:
     # await task
 
 
-asyncio.run(main())
+if float(python_version) >= 3.7:
+    asyncio.run(main())
+else:
+    loop = asyncio.get_event_loop()
+    task = asyncio.ensure_future(main())
+    loop.run_until_complete(task)
--- asyncio并发编程/future获取返回值.py
+++ asyncio并发编程/future获取返回值.py
@@ -1,5 +1,8 @@
 # -*- coding:utf-8 -*-
 import asyncio
+import platform
+import selectors
+import sys
 import threading
 
 
@@ -11,6 +14,21 @@ async def get_html(url):
 
 
 if __name__ == '__main__':
+    # selector = selectors.SelectSelector()
+
+    # 关于selectors
+    # def accept(sock, mask):
+    #     pass
+    #     # 注册conn，回调 read函数
+    #     sel.register(conn, selectors.EVENT_READ, read)
+    # 注册server事件：
+    # 参数一：sock 进行监听
+    # 参数二：selectors.EVENT_READ 执行动作
+    # 参数三：accept,只要来一个链接就回调这个函数
+    # sel.register(sock, selectors.EVENT_READ, accept)
+
+    # loop = asyncio.SelectorEventLoop(selector)
+    # asyncio.set_event_loop(loop)
     loop = asyncio.get_event_loop()
     # 创建future对象，和asyncio中future对象不一样，此对象不可等待
     # loop_future = loop.create_future()
@@ -23,9 +41,19 @@ if __name__ == '__main__':
     # 在asyncio中需要Future对象以便允许通过 async / await 使用基于回调的代码。
     # 通常情况下没有必要在应用层级的代码中创建Future对象。
     # Future对象有时会由库和某些asyncioAPI暴露给用户，用作可等待对象
-    future = asyncio.ensure_future(get_html("www.baidu.com"))
-    loop.run_until_complete(future)
-    res = future.result()  # 获取返回值
+
+    # 使用与所有Python版本
+    task = asyncio.ensure_future(get_html("www.baidu.com"))
+    # print(sys.version.split(" ")[0].rpartition(".")[0])
+    # print(sys.version_info)
+
+    # python_version = platform.python_version().rpartition(".")[0]
+    # if float(python_version) >= 3.7:
+    #     task = asyncio.create_task(get_html("www.baidu.com"))
+    # else:
+    #     task = asyncio.ensure_future(get_html("www.baidu.com"))
+    loop.run_until_complete(task)
+    res = task.result()  # 获取返回值
     # 或者
     # task = loop.create_task(get_html("www.baidu.com"))
     # loop.run_until_complete(task)
